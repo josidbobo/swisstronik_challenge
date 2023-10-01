3660d0f10486522e1aca1742adfad6bc62672c4e
Joshua Okoro <josidbobo@gmail.com>
2023-10-01 03:02:37 +0100

    chore: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import ("dotenv").config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    swisstronik: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
